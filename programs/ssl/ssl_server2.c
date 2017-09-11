eed4819972471d3863880fabfafcbc71a1ec8505
Hannes Tschofenig <Hannes.Tschofenig@arm.com>
2017-09-11 12:26:13 +0200

    Changing the default settings and the timeout.

--- programs/ssl/ssl_server2.c
+++ programs/ssl/ssl_server2.c
@@ -120,7 +120,7 @@ int main( void )
 #define DFL_ARC4                -1
 #define DFL_AUTH_MODE           -1
 #define DFL_MFL_CODE            MBEDTLS_SSL_MAX_FRAG_LEN_NONE
-#define DFL_RSL                 0 
+#define DFL_RSL                 MBEDTLS_SSL_RECORD_SIZE_LIMIT_NONE
 #define DFL_TRUNC_HMAC          -1
 #define DFL_TICKETS             MBEDTLS_SSL_SESSION_TICKETS_ENABLED
 #define DFL_TICKET_TIMEOUT      86400
@@ -230,7 +230,7 @@ int main( void )
 
 #if defined(MBEDTLS_SSL_RECORD_SIZE_LIMIT )
 #define USAGE_RECORD_SIZE_LIMIT                                      \
-    "    record_size_limit=%%d     default: 16384 (tls default)\n"  
+    "    record_size_limit=%%d     default: 0 (extension not used)\n"  
 #else
 #define USAGE_RECORD_SIZE_LIMIT ""
 #endif /* MBEDTLS_SSL_RECORD_SIZE_LIMIT */
@@ -1145,6 +1145,7 @@ int main( int argc, char *argv[] )
 		else if (strcmp(p, "record_size_limit") == 0)
 		{
 			opt.rsl = atoi(q);
+			if (opt.rsl < 0 || opt.rsl> 16384) goto usage;
 		}
         else if( strcmp( p, "max_frag_len" ) == 0 )
         {
@@ -2008,9 +2009,14 @@ handshake:
                     (unsigned int) mbedtls_ssl_get_max_frag_len( &ssl ) );
 #endif
 
-#if defined(MBEDTLS_SSL_RECORD_SIZE_LIMIT )
-	mbedtls_printf("    [ Record Size Limit is %u bytes]\n",
-		(unsigned int)mbedtls_ssl_get_record_size_limit(&ssl));
+#if defined(MBEDTLS_SSL_RECORD_SIZE_LIMIT)
+	if ((unsigned int)mbedtls_ssl_get_record_size_limit(&ssl) == MBEDTLS_SSL_RECORD_SIZE_LIMIT_NONE) {
+		mbedtls_printf("    [ Record Size Limit has not been negotiated ]\n");
+	}
+	else {
+		mbedtls_printf("    [ Record Size Limit is %u bytes ]\n",
+			(unsigned int)mbedtls_ssl_get_record_size_limit(&ssl));
+	}
 #endif
 
 #if defined(MBEDTLS_SSL_ALPN)
