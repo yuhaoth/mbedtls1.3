a2617d14cbccbe6fc99772acf6dfd3ac9d6bfae3
Hanno Becker <hanno.becker@arm.com>
2020-05-08 06:50:46 +0100

    Crypto changes for TLS 1.3 prototype

--- library/cipher.c
+++ library/cipher.c
@@ -1353,7 +1353,11 @@ int mbedtls_cipher_auth_encrypt( mbedtls_cipher_context_t *ctx,
     }
 #endif /* MBEDTLS_GCM_C */
 #if defined(MBEDTLS_CCM_C)
-    if( MBEDTLS_MODE_CCM == ctx->cipher_info->mode )
+    if( MBEDTLS_MODE_CCM == ctx->cipher_info->mode 
+#if defined(MBEDTLS_SSL_PROTO_TLS1_3_EXPERIMENTAL)
+        || ctx->cipher_info->mode == MBEDTLS_MODE_CCM_8
+#endif /* MBEDTLS_SSL_PROTO_TLS1_3_EXPERIMENTAL */
+        )
     {
         *olen = ilen;
         return( mbedtls_ccm_encrypt_and_tag( ctx->cipher_ctx, ilen,
@@ -1464,7 +1468,11 @@ int mbedtls_cipher_auth_decrypt( mbedtls_cipher_context_t *ctx,
     }
 #endif /* MBEDTLS_GCM_C */
 #if defined(MBEDTLS_CCM_C)
-    if( MBEDTLS_MODE_CCM == ctx->cipher_info->mode )
+    if( MBEDTLS_MODE_CCM == ctx->cipher_info->mode 
+#if defined(MBEDTLS_SSL_PROTO_TLS1_3_EXPERIMENTAL)        
+        || ctx->cipher_info->mode == MBEDTLS_MODE_CCM_8
+#endif /* MBEDTLS_SSL_PROTO_TLS1_3_EXPERIMENTAL */
+        )
     {
         int ret = MBEDTLS_ERR_ERROR_CORRUPTION_DETECTED;
 
