fe2ca078ad98b5c9521bc0250f2e646bbd37d20c
Guilhem Bryant <Guilhem.Bryant@arm.com>
2020-06-16 12:27:05 +0100

    Fix return value check of `mbedtls_ssl_get_psk()` in client PSK identity extension parsing

--- library/ssl_tls13_server.c
+++ library/ssl_tls13_server.c
@@ -1045,8 +1045,8 @@ psk_parsing_successful:
         }
         else {
             /* Case 2: We are using a static PSK, or a dynamic PSK if one is defined */
-            if( mbedtls_ssl_get_psk( ssl, &psk, &psk_len ) == MBEDTLS_ERR_SSL_PRIVATE_KEY_REQUIRED )
-                return( MBEDTLS_ERR_SSL_PRIVATE_KEY_REQUIRED );
+            if( ( ret = mbedtls_ssl_get_psk( ssl, &psk, &psk_len ) ) != 0 )
+                return( ret );
 
             ret = ssl_calc_binder( ssl, (unsigned char *) psk, psk_len,
                                   mbedtls_md_info_from_type( ssl->transform_negotiate->ciphersuite_info->mac ), ssl->transform_negotiate->ciphersuite_info,
