22ddc8fc7883090ebacfbc88aad4e17d60b7cdfc
Hannes Tschofenig <hannes.tschofenig@arm.com>
2020-10-08 21:29:36 +0200

    Added mbedtls_ssl_conf_signature_algorithms() API

--- library/ssl_tls13_generic.c
+++ library/ssl_tls13_generic.c
@@ -4440,8 +4440,13 @@ int mbedtls_ssl_conf_ticket_meta( mbedtls_ssl_config *conf,
 
 #endif /* MBEDTLS_SSL_NEW_SESSION_TICKET */
 
-
-
+#if defined(MBEDTLS_X509_CRT_PARSE_C)
+void mbedtls_ssl_conf_signature_algorithms( mbedtls_ssl_config *conf,
+                     const int* sig_algs )
+{
+    conf->sig_hashes = sig_algs; 
+}
+#endif /* MBEDTLS_X509_CRT_PARSE_C */
 
 #if defined(MBEDTLS_SSL_NEW_SESSION_TICKET)
 
