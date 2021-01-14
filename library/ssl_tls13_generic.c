af91d01f00b5159884396453169961e0247b8726
lhuang04 <lhuang04@fb.com>
2021-01-14 08:15:43 -0800

    Check `mbedtls_ssl_get_psk` before we set MBEDTLS_SSL_EARLY_DATA_ON
    Summary:
    In early data mode, the `ssl->handshake->ciphersuite_info` is [set](https://github.com/hannestschofenig/mbedtls/blob/tls13-prototype/library/ssl_tls13_client.c#L1007) in `mbedtls_ssl_write_pre_shared_key_ext` after check [mbedtls_ssl_get_psk](https://github.com/hannestschofenig/mbedtls/blob/tls13-prototype/library/ssl_tls13_client.c#L976).
    If psk is not configured, then `ssl->handshake->ciphersuite_info` will
    not be set. However, `mbedtls_ssl_generate_early_data_keys` assumes that
    should be always be set.
    
    This PR checks `mbedtls_ssl_get_psk` before we set
    MBEDTLS_SSL_EARLY_DATA_ON in `mbedtls_ssl_write_early_data_ext`. This
    avoid calling into `mbedtls_ssl_generate_early_data_keys` and other
    functions that are running in early data mode.
    
    Test Plan:
    ```
    ../programs/ssl/ssl_server2 server_addr=127.0.0.1 server_port=11252 allow_sha1=1 debug_level=5 force_version=tls1_3
    ```
    
    ```
    ../programs/ssl/ssl_client2 server_addr=127.0.0.1 server_port=11252 allow_sha1=1 debug_level=5 force_version=tls1_3 force_ciphersuite=TLS_AES_128_CCM_SHA256 early_data=1
    ```
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- library/ssl_tls13_generic.c
+++ library/ssl_tls13_generic.c
@@ -3117,6 +3117,7 @@ int mbedtls_ssl_write_early_data_ext( mbedtls_ssl_context *ssl,
     if( ssl->conf->endpoint == MBEDTLS_SSL_IS_CLIENT )
     {
         if( ssl->conf->key_exchange_modes == MBEDTLS_SSL_TLS13_KEY_EXCHANGE_MODE_ECDHE_ECDSA ||
+            mbedtls_ssl_get_psk( ssl, NULL, NULL ) != 0 ||
             ssl->early_data_enabled == MBEDTLS_SSL_EARLY_DATA_DISABLED )
         {
             MBEDTLS_SSL_DEBUG_MSG( 2, ( "<= skip write early_data extension" ) );
