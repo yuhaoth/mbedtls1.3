8ba2fc93a74ffb7fea38d3266ae2023cd0ee3b4e
Hanno Becker <hanno.becker@arm.com>
2021-02-05 21:02:56 +0000

    Fix ill-formed debug statement in record decryption
    Signed-off-by: Hanno Becker <hanno.becker@arm.com>

--- library/ssl_msg.c
+++ library/ssl_msg.c
@@ -1472,7 +1472,9 @@ int mbedtls_ssl_decrypt_buf( mbedtls_ssl_context const *ssl,
         /* Check that there's space for the authentication tag. */
         if( rec->data_len < transform->taglen )
         {
-            MBEDTLS_SSL_DEBUG_MSG( 1, ( "msglen (%d) < taglen (%d) " ) );
+            MBEDTLS_SSL_DEBUG_MSG( 1, ( "msglen (%d) < taglen (%d) ",
+                                        (unsigned) rec->data_len,
+                                        (unsigned) transform->taglen ) );
             return( MBEDTLS_ERR_SSL_INVALID_MAC );
         }
 
