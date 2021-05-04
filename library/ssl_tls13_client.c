05b068ea4cb6ded1e32148f28c4519284d4566ce
Zhi Han <zhih@fb.com>
2021-05-04 11:44:25 -0400

    Add  MBEDTLS_SSL_TLS13_COMPATIBILITY_MODE back

--- library/ssl_tls13_client.c
+++ library/ssl_tls13_client.c
@@ -423,12 +423,13 @@ static int ssl_write_end_of_early_data_coordinate( mbedtls_ssl_context* ssl )
 
 static int ssl_write_end_of_early_data_postprocess( mbedtls_ssl_context* ssl )
 {
+#if defined(MBEDTLS_SSL_TLS13_COMPATIBILITY_MODE)
     if( ssl_write_end_of_early_data_coordinate( ssl ) != SSL_END_OF_EARLY_DATA_WRITE )
     {
         mbedtls_ssl_handshake_set_state( ssl, MBEDTLS_SSL_CLIENT_CCS_AFTER_SERVER_FINISHED ); 
         return( 0 );
     }
-
+#endif /* MBEDTLS_SSL_TLS13_COMPATIBILITY_MODE */ 
     mbedtls_ssl_handshake_set_state( ssl, MBEDTLS_SSL_CLIENT_CERTIFICATE );
     return( 0 );
 }
