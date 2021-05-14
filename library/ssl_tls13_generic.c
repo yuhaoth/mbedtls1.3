ebfc4983648f649a8f357409a2e35080bd081355
lhuang04 <lhuang04@fb.com>
2021-05-14 08:48:30 -0700

    Change to mbedtls_ssl_conf_tls13_some_psk_enabled
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- library/ssl_tls13_generic.c
+++ library/ssl_tls13_generic.c
@@ -3116,7 +3116,7 @@ int mbedtls_ssl_write_early_data_ext( mbedtls_ssl_context *ssl,
 #if defined(MBEDTLS_SSL_CLI_C)
     if( ssl->conf->endpoint == MBEDTLS_SSL_IS_CLIENT )
     {
-        if( ssl->conf->key_exchange_modes == MBEDTLS_SSL_TLS13_KEY_EXCHANGE_MODE_ECDHE_ECDSA ||
+        if( !mbedtls_ssl_conf_tls13_some_psk_enabled( ssl ) ||
             mbedtls_ssl_get_psk( ssl, NULL, NULL ) != 0 ||
             ssl->early_data_enabled == MBEDTLS_SSL_EARLY_DATA_DISABLED )
         {
