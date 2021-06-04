0871ccb623b5799e51713730d3a53d9afd78eb00
Zhi Han <zhih@fb.com>
2021-06-04 11:26:00 -0400

    Add <= to log

--- library/ssl_tls13_generic.c
+++ library/ssl_tls13_generic.c
@@ -3105,7 +3105,7 @@ int mbedtls_ssl_write_early_data_ext( mbedtls_ssl_context *ssl,
         if( ssl->conf->key_exchange_modes != MBEDTLS_SSL_TLS13_KEY_EXCHANGE_MODE_PSK_KE ||
             ssl->conf->early_data_enabled == MBEDTLS_SSL_EARLY_DATA_DISABLED )
         {
-            MBEDTLS_SSL_DEBUG_MSG( 2, ( "skip write early_data extension" ) );
+            MBEDTLS_SSL_DEBUG_MSG( 2, ( "<= skip write early_data extension" ) );
             ssl->handshake->early_data = MBEDTLS_SSL_EARLY_DATA_OFF;
             return( 0 );
         }
