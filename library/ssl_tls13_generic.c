1790fa932641b50a8354b2950bca83bcc53736bf
Zhi Han <zhi.han@gmail.com>
2021-06-08 15:06:32 -0400

    Update library/ssl_tls13_generic.c
    Review feedback
    
    Co-authored-by: Hanno Becker <hanno.becker@arm.com>

--- library/ssl_tls13_generic.c
+++ library/ssl_tls13_generic.c
@@ -3102,7 +3102,8 @@ int mbedtls_ssl_write_early_data_ext( mbedtls_ssl_context *ssl,
         if( ( ssl->handshake->extensions_present & EARLY_DATA_EXTENSION ) == 0 )
             return( 0 );
 
-        if( ssl->conf->key_exchange_modes != MBEDTLS_SSL_TLS13_KEY_EXCHANGE_MODE_PSK_KE ||
+        if( ssl->conf->key_exchange_modes != 
+                   MBEDTLS_SSL_TLS13_KEY_EXCHANGE_MODE_PSK_KE ||
             ssl->conf->early_data_enabled == MBEDTLS_SSL_EARLY_DATA_DISABLED )
         {
             MBEDTLS_SSL_DEBUG_MSG( 2, ( "<= skip write early_data extension" ) );
