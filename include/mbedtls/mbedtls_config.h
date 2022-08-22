595ac4fabf41570c2a6f0b020d71b0a45512c07c
Jerry Yu <jerry.h.yu@arm.com>
2022-08-22 14:22:17 +0800

    Change set session and psk definition
    Signed-off-by: Jerry Yu <jerry.h.yu@arm.com>

--- include/mbedtls/mbedtls_config.h
+++ include/mbedtls/mbedtls_config.h
@@ -1545,6 +1545,13 @@
 //#define MBEDTLS_SSL_TLS1_3_COMPATIBILITY_MODE
 
 /**
+ * \brief MBEDTLS_SSL_TLS1_3_MAX_PSK_SLOTS
+ *
+ * Max slots for saving PSK and session tickets.
+ */
+#define MBEDTLS_SSL_TLS1_3_MAX_PSK_SLOTS 16
+
+/**
  * \def MBEDTLS_SSL_TLS1_3_TICKET_NONCE_LENGTH
  *
  * Size in bytes of a ticket nonce. This is not used in TLS 1.2.
